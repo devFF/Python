2cf11047f4faacc7c89ccd4d42135fcc0a2caff0
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-10-19 20:35:13 +0500

    Finish

--- Python/Parsing/skillbox/parser_telegram_bot/head_hunter.py
+++ Python/Parsing/skillbox/parser_telegram_bot/head_hunter.py
@@ -1,37 +1,44 @@
 from selenium import webdriver  # импорт движка
+from selenium.webdriver.chrome.options import Options
 import chromedriver_binary  # прослойка между кодом и браузером, установить версию == версии браузера
 import time
 from selenium.webdriver.common.by import By  # для поиска по CSS-селекторам
 import re
 
-browser = webdriver.Chrome()  # Запуск браузера
-browser.get('http://hh.ru')  # Открываем hh.ru
 
-# Найдем поле поискового запроса и введем текст
-# Данное поле точно характеризует id (см. веб-инспектор хрома), поэтому определяем поле по нему
-search_input = browser.find_element(By.ID, 'a11y-search-input')
-search_input.send_keys('Junior Python')  # Отправили текст в поле
+def launch_parser(title):
+    options = Options()
+    options.headless = True
+    browser = webdriver.Chrome(options=options)  # Запуск браузера
+    browser.get('http://hh.ru')  # Открываем hh.ru
 
-# Найдем кнопку поиска и нажмем на нее
-search_button = browser.find_element(By.CSS_SELECTOR, '[data-qa="search-button"]')
-search_button.click()
+    # Найдем поле поискового запроса и введем текст
+    # Данное поле точно характеризует id (см. веб-инспектор хрома), поэтому определяем поле по нему
+    search_input = browser.find_element(By.ID, 'a11y-search-input')
+    search_input.send_keys(title)  # Отправили текст в поле
 
-# Найдем заголовок с количеством вакансий и выведем их количество
-"""<div data-qa="vacancies-search-header">
-<div class="bloko-v-spacing bloko-v-spacing_base-6"></div>
-<h1 data-qa="bloko-header-3" class="bloko-header-section-3">
-7 
-<span>вакансий</span> 
-«Junior Python»
-</h1>
-<div class="bloko-v-spacing bloko-v-spacing_base-4"></div></div>
-"""
-# исходя из структуры видно, что данные нужно получить по селектору по объявлению заголовка h1 - объявим чз пробел
-vacancy_number = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"] h1').text
+    # Найдем кнопку поиска и нажмем на нее
+    search_button = browser.find_element(By.CSS_SELECTOR, '[data-qa="search-button"]')
+    search_button.click()
 
-# Заменим все символы, не являющиеся цифрой
-count = re.sub(r'\D', '', vacancy_number)
+    # Найдем заголовок с количеством вакансий и выведем их количество
+    """<div data-qa="vacancies-search-header">
+    <div class="bloko-v-spacing bloko-v-spacing_base-6"></div>
+    <h1 data-qa="bloko-header-3" class="bloko-header-section-3">
+    7 
+    <span>вакансий</span> 
+    «Junior Python»
+    </h1>
+    <div class="bloko-v-spacing bloko-v-spacing_base-4"></div></div>
+    """
+    # исходя из структуры видно, что данные нужно получить по селектору по объявлению заголовка h1 - объявим чз пробел
+    vacancy_number = browser.find_element(By.CSS_SELECTOR, '[data-qa="vacancies-search-header"] h1').text
 
-print(f"Количество вакансий с запросом «Junior Python»: {count}")
+    # Заменим все символы, не являющиеся цифрой
+    count = re.sub(r'\D', '', vacancy_number)
 
-browser.close()
+    # print(f"Количество вакансий с запросом «Junior Python»: {count}")
+
+    browser.close()
+
+    return count
--- Python/Parsing/skillbox/parser_telegram_bot/telegram_bot.py
+++ Python/Parsing/skillbox/parser_telegram_bot/telegram_bot.py
@@ -1,15 +1,24 @@
 from telegram import Update  # Update -- кусок информации, полученный от телеграма
 from telegram.ext import ApplicationBuilder, MessageHandler, filters  # Способ создать приложение с указанием настроек
-
+from head_hunter import launch_parser
 
 # token - секретный ключ к боту, получить у @BotFather
 app = ApplicationBuilder().token('5651820426:AAEtEE_ZhpmW9l-OTXc0VTRHSF5qnICnjO8').build()
 
+
 # upd - новая информация
 # ctx - контекст, служебная информация
 # После двоеточия тип данных
-def text_reply(upd: Update, ctx):
-    pass
+async def text_reply(upd: Update, ctx):
+    user_text = upd.message.text
+    print(f'User: {user_text}')
+    # Запуск парсинга
+    name = upd.message.from_user.full_name
+    answer = f'Уважаемый, {name}, мы получили Ваш запрос "{user_text}"'
+    await upd.message.reply_text(answer)
+    count = launch_parser(title=user_text)
+    answer = f'Найдено {count} вакансий'
+    await upd.message.reply_text(answer)
 
 
 # обработчик сообщений
@@ -20,12 +29,3 @@ app.add_handler(handler)
 
 # запуск приложения
 app.run_polling()
-
-
-
-
-
-
-
-
-
