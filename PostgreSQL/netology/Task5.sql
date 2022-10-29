a4537776227d09738b6625054328f6691783a67c
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-10-29 17:27:47 +0500

    Renamed

--- PostgreSQL/netology/Task5.sql
+++ PostgreSQL/netology/Task5.sql
@@ -1,27 +1,31 @@
--- отношение стоимости товаров к единице стоимости этих товаров 
--- сумма платежей в разрезе каждого товара
-select opl.product_id, sum(o.amount)
+--оконные функции
+--отношение стоимости товаров к единице стоимости этих товаров
+-- over открывает оконную функцию
+-- partition by - группировка по индентификатору товара
+-- distinct - удаляет дублирующиеся функции
+
+select distinct p.product_id, sum(o.amount) over (partition by p.product_id) / price
 from orders o 
 join order_product_list opl on o.order_id = opl.order_id 
-group by opl.product_id 
+join product p on p.product_id = opl.product_id 
 
--- информация по сумме каждого продукта
-select p.product_id, p.price 
-from product p 
+-- получим данные о каждом тысячном заказе
 
--- совместим две логики
-select t1.product_id, sum / price
-from (select opl.product_id, sum(o.amount)
+-- для начала напишем функцию запроса данных с группировкой по номеру заказа
+-- оконная функция row_number, которая проставит порядковые номера для наших записей
+select o.order_id, o.customer_id, o.amount ,
+	row_number () over (order by order_id)
 from orders o 
-join order_product_list opl on o.order_id = opl.order_id 
-group by opl.product_id ) t1
-join (select p.product_id, p.price 
-from product p ) t2 on t1.product_id = t2.product_id
 
--- или
-select t1.product_id, sum / (select p.price from product p where p.product_id = t1.product_id)
-from (select opl.product_id, sum(o.amount)
+-- запишем ее как функцию и добавим условие
+select *
+from (select o.order_id, o.customer_id, o.amount ,
+	row_number () over (order by order_id) 
+from orders o) t
+where t.row_number % 1000 = 0 -- без остатка делится на 1000
+
+-- получим накопительную сумму платежей по каждому пользователю
+select o.order_id, o.customer_id , o.amount,
+	sum(amount) over (partition by customer_id order by order_id)
 from orders o 
-join order_product_list opl on o.order_id = opl.order_id 
-group by opl.product_id ) t1
 
--- PostgreSQL/netology/Task6.sql
+++ PostgreSQL/netology/Task6.sql
@@ -1,31 +1,43 @@
---оконные функции
---отношение стоимости товаров к единице стоимости этих товаров
--- over открывает оконную функцию
--- partition by - группировка по индентификатору товара
--- distinct - удаляет дублирующиеся функции
+-- представления
 
-select distinct p.product_id, sum(o.amount) over (partition by p.product_id) / price
-from orders o 
-join order_product_list opl on o.order_id = opl.order_id 
-join product p on p.product_id = opl.product_id 
-
--- получим данные о каждом тысячном заказе
-
--- для начала напишем функцию запроса данных с группировкой по номеру заказа
--- оконная функция row_number, которая проставит порядковые номера для наших записей
-select o.order_id, o.customer_id, o.amount ,
-	row_number () over (order by order_id)
-from orders o 
-
--- запишем ее как функцию и добавим условие
+-- нужно постоянно получать данные по последнему заказу пользователя
+-- для начала реализуем логику по последнему зазказу пользователя 
+-- (обратная сортировка по убыванию, берем только 1 элемент)
 select *
-from (select o.order_id, o.customer_id, o.amount ,
-	row_number () over (order by order_id) 
+from (
+select o.order_id , o.customer_id , o.amount ,
+	row_number() over (partition by o.customer_id order by order_id desc)
 from orders o) t
-where t.row_number % 1000 = 0 -- без остатка делится на 1000
+where row_number = 1
 
--- получим накопительную сумму платежей по каждому пользователю
-select o.order_id, o.customer_id , o.amount,
-	sum(amount) over (partition by customer_id order by order_id)
-from orders o 
+-- создадим представление на основе предыдущей логики
+create view task7 as 
+select *
+from (
+select o.order_id , o.customer_id , o.amount ,
+	row_number() over (partition by o.customer_id order by order_id desc)
+from orders o) t
+where row_number = 1
 
+-- вызовем представление 
+select * from task7
+
+-- создадим материализованное представление (таблица запишется в память на жесткий диск)
+create materialized view task7m as 
+select *
+from (
+select o.order_id , o.customer_id , o.amount ,
+	row_number() over (partition by o.customer_id order by order_id desc)
+from orders o) t
+where row_number = 1
+with no data -- но данными не заполняем 
+
+-- запросим данные из матер представления, но получим ошибку, 
+-- так как не заполнена разметка на диске (нет данных)
+select * from task7m
+
+--обновим данные в материализованном представлении
+refresh materialized view task7m 
+
+-- удалим материализованное представление
+drop materialized view task7m 
