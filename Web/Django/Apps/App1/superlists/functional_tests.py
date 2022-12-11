f639742759e0e9641d3fcaee2708eb650ba7682d
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-12-11 19:29:55 +0500

    add unittest

--- Web/Django/Apps/App1/superlists/functional_tests.py
+++ Web/Django/Apps/App1/superlists/functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетителя"""
+
+    def setUp(self):
+        """установка"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """демонтаж"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """тест: можно начать список и получить его позже"""
+        # Эдит слышала про крутое новое онлайн-приложение со
+        # списком неотложных дел. Она решает оценить его домашнюю страницу
+        self.browser.get('http://localhost:8000')
+        # Она видит, что заголовок и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+        # Ей сразу же предлагается ввести элемент списка
+        # [...остальные комментарии, как и прежде]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
