3dc122a56f808c15745b528db08be00dc5942d8f
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-12-11 20:09:03 +0500

    Тест url
    Первый модульный тест и преобразование url, фиктивное
    представление

--- Web/Django/Apps/App1/superlists/lists/tests.py
+++ Web/Django/Apps/App1/superlists/lists/tests.py
@@ -1,9 +1,17 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-
-class SmokeTest(TestCase):
-    """Тест на токсичность"""
-
-    def test_bad_maths(self):
-        """Тест: неправильные математические расчеты"""
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    """тест домашней страницы"""
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление домашней страницы"""
+        """resolve – это функция, которую Django использует внутренне для 
+        преобразования URL-адреса и нахождения функций представления,
+        в соответствие которым они должны быть поставлены. Мы проверяем, чтобы resolve, когда ее вызывают с «/», 
+        то есть корнем сайта, нашла функцию под названием home_page."""
+        """Что это за функция? Это функция представления, которую мы собираемся написать далее, и она вернет 
+        фактический HTML, который нам нужен. Из оператора import вы видите, что мы планируем сохранить 
+        ее в lists/views.py."""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
