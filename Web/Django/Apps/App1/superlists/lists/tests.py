9e8189045a3140b563887198911658b78b52aef7
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-12-11 19:45:56 +0500

    SmokeTest
    Добавлено приложение для списков с намеренно неработающим модульным тестом

---
+++ Web/Django/Apps/App1/superlists/lists/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class SmokeTest(TestCase):
+    """Тест на токсичность"""
+
+    def test_bad_maths(self):
+        """Тест: неправильные математические расчеты"""
+        self.assertEqual(1 + 1, 3)
