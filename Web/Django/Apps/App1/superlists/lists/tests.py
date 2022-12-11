3cc85da820c2d4e89bec8838de0f254b464eb59e
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2022-12-11 20:39:14 +0500

    "Базовое представление теперь возвращает минимальный HTML

--- Web/Django/Apps/App1/superlists/lists/tests.py
+++ Web/Django/Apps/App1/superlists/lists/tests.py
@@ -1,17 +1,20 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
 
+
 class HomePageTest(TestCase):
-    """тест домашней страницы"""
     def test_root_url_resolves_to_home_page_view(self):
         """тест: корневой url преобразуется в представление домашней страницы"""
-        """resolve – это функция, которую Django использует внутренне для 
-        преобразования URL-адреса и нахождения функций представления,
-        в соответствие которым они должны быть поставлены. Мы проверяем, чтобы resolve, когда ее вызывают с «/», 
-        то есть корнем сайта, нашла функцию под названием home_page."""
-        """Что это за функция? Это функция представления, которую мы собираемся написать далее, и она вернет 
-        фактический HTML, который нам нужен. Из оператора import вы видите, что мы планируем сохранить 
-        ее в lists/views.py."""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """тест: домашняя страница возвращает правильный html"""
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
